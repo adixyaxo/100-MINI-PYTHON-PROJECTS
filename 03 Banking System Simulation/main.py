54760e2471db4c4cef876da7d6a75165749a7836
adixyxo <aditya0dagar@gmail.com>
2026-01-09 18:52:59 +0530

    writing choice system

--- 03 Banking System Simulation/main.py
+++ 03 Banking System Simulation/main.py
@@ -1,4 +1,6 @@
-import pandas as pd
+import pandas
+import time
+import datetime
 import openpyxl
 class account:
     def __init__(self,name,account_no,balance,loan,):
@@ -9,7 +11,7 @@ class account:
         self.account_info =  {'NAME' : self.name,'ACCOUNT NO' : self.account_no,'BALANCE' : self.balance,'LOAN' : self.loan}
     
     def save_info(self):
-        df = pd.DataFrame(self.account_info,index=[0])
+        df = pandas.DataFrame(self.account_info,index=[0])
         df.to_excel(f"03 Banking System Simulation/{self.account_no}.xlsx",index=False)
 
     def getinfo(self):
@@ -37,8 +39,46 @@ aditya = savings_account("aditya",727,10000,10,21)
 aditya.getinfo()
 
 def main():
-    print(aditya.account_info)
-    aditya.save_info()
+    print("""
+    ________________________________________________
+    |                                              |
+    |          $$$  IRON BANK SYSTEM  $$$          |
+    |______________________________________________|
+    |                                              |
+    |   1. Create Account      2. Login            |
+    |   3. Deposit             4. Withdraw         |
+    |   5. Check Balance       6. Transaction Log  |
+    |   7. Exit                                    |
+    |______________________________________________|
+    """)
 
+    user_choice = input("ENTER YOUR CHOICE :: ")
+    try:
+        user_choice_int = int(user_choice)
+    except Exception as e:
+        pass
+    
+    user_choice = user_choice.strip()
+    
+    if user_choice.lower() == 'create account':
+        user_choice_int = 1
+    elif user_choice.lower() == 'login':
+        user_choice_int = 2
+    elif user_choice.lower() == 'deposit':
+        user_choice_int = 3
+    elif user_choice.lower() == 'withdraw':
+        user_choice_int = 4
+    elif user_choice.lower() == 'check balance' or user_choice.lower() == 'checkbalance':
+        user_choice_int = 5
+    elif user_choice.lower() == 'transaction log' or user_choice.lower() == 'transactionlog':
+        user_choice_int = 6
+    elif user_choice.lower() == 'exit':
+        user_choice_int = 7
+    
+    if user_choice_int !=1 and user_choice_int !=2 and user_choice_int !=3 and user_choice_int !=4 and user_choice_int !=5 and user_choice_int !=6 and user_choice_int !=7:
+        print("PLEASE ONLY ENTER FROM ABOVE CHOICES")
+    
+    
+    
 if __name__ == '__main__':
     main()
