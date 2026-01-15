3a4f9f245891a04b58fc081349a6bfd90cbebdcb
adixyxo <aditya0dagar@gmail.com>
2026-01-15 23:40:03 +0530

    creating a class vehicle with all the vehicle information

--- 05 Smart Parking Lot.py
+++ 05 Smart Parking Lot.py
@@ -2,3 +2,18 @@
     
 #       - Classes: Vehicle (plate, type), Ticket (timestamp), ParkingLot.
 #       - Features: Calculate fee based on time, manage spot availability.
+
+import time
+
+#  The cost of the ticket would be calculated on the based of the entry and exit time of the vechicle also taking in consideration the type of vehicle.
+
+class vehicle:
+    def __init__(self, plate, type, color, model, owner_name):
+        self.plate = plate
+        self.type = type #this will register the type of vehicle eg car, bike, truck
+        self.color = color
+        self.model = model
+        self.owner_name = owner_name
+        self.entry_time = time.time()
+
+class ticket:
