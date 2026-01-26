c63e9ab2f0c89fea7cdaab23a77e5979cf684ea2
Aditya <aditya0dagar@gmail.com>
2026-01-26 16:14:17 +0530

    writing showing of tasks logic and csv logic also

--- Flask Projects/Todo App/main.py
+++ Flask Projects/Todo App/main.py
@@ -3,6 +3,11 @@ import pandas as pd
 
 app = Flask(__name__)
 
+def get_tasks():
+    tasks_list = pd.read_csv("tasks.csv")
+    tasks_list = tasks_list.to_dict()
+    return tasks_list
+
 class task:
     def __init__(self,name,description,due_date,date_added,status=False):
         self.name = name
@@ -10,19 +15,17 @@ class task:
         self.due_date = due_date
         self.status = status
         self.date_added = date_added
-        
-        
-
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",task_list = get_tasks())
 
 @app.route("/add",methods = ["GET","POST"])
 def add():
     if request.method == "POST":
-        print(request.form)
+        task_info = request.form
+        name = task_info["name"]
     return render_template("index.html")    
 
 
